e3c9c28c03bb630aa475417b9028ad3e802f6f8f
Deepak Sharma <deepak.sharma@globant.com>
2026-06-25 22:46:34 +0530

    initial commit

---
+++ src/test/java/com/sms/sensor_measurement_system/SensorMeasurementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.sensor_measurement_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMeasurementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
